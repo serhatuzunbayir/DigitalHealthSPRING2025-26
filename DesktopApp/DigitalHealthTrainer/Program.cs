bb6be480c51d4521853d7643e861a46c038f22b0
Efe Serin <efe.serin999@gmail.com>
2026-05-01 02:12:05 +0300

    desktopApp Solution setup.

---
+++ DesktopApp/DigitalHealthTrainer/Program.cs
@@ -0,0 +1,14 @@
+using DigitalHealthTrainer.Forms;
+
+namespace DigitalHealthTrainer
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginForm());
+        }
+    }
+}
